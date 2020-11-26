acb45540c21acca3db7df9c3ad3c786c55be4419
Roberto Di Remigio <roberto.diremigio@gmail.com>
2020-11-26 14:10:45 +0100

    Add niv

--- the-press.nix
+++ the-press.nix
@@ -1,5 +1,8 @@
-{ pkgs, ... }:
-
+{ system ? builtins.currentSystem }:
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs { inherit system; };
+in
 pkgs.dockerTools.buildImage {
   contents = pkgs.hello;
   config.Cmd = [ "/bin/hello" ];
