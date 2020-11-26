bba9b6b44527df7e25d716fa5861ad4e66c47259
Roberto Di Remigio <roberto.diremigio@gmail.com>
2020-11-26 19:09:04 +0100

    All together now!

--- the-press.nix
+++ the-press.nix
@@ -16,28 +16,28 @@ pkgs.dockerTools.buildImage {
       texlive.combine {
         inherit (texlive)
           collection-basic
-    #      collection-bibtexextra
-    #      collection-binextra
-    #      collection-context
-    #      collection-fontsextra
-    #      collection-fontsrecommended
-    #      collection-fontutils
-    #      collection-formatsextra
-    #      collection-langenglish
-    #      collection-langeuropean
-    #      collection-langfrench
-    #      collection-langitalian
-    #      collection-langother
+          collection-bibtexextra
+          collection-binextra
+          collection-context
+          collection-fontsextra
+          collection-fontsrecommended
+          collection-fontutils
+          collection-formatsextra
+          collection-langenglish
+          collection-langeuropean
+          collection-langfrench
+          collection-langitalian
+          collection-langother
           collection-latex
-    #      collection-latexextra
-    #      collection-latexrecommended
-    #      collection-luatex
-    #      collection-mathscience
-    #      collection-metapost
-    #      collection-pictures
-    #      collection-plaingeneric
-    #      collection-pstricks
-    #      collection-publishers
+          collection-latexextra
+          collection-latexrecommended
+          collection-luatex
+          collection-mathscience
+          collection-metapost
+          collection-pictures
+          collection-plaingeneric
+          collection-pstricks
+          collection-publishers
           collection-xetex
           ;
       }
