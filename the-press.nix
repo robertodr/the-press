dd3e4dab09a0a3975a382209b2d545bec2bc4807
Roberto Di Remigio <roberto.diremigio@gmail.com>
2021-02-17 09:21:18 +0100

    Add combined image

---
+++ the-press.nix
@@ -0,0 +1,39 @@
+{ system ? builtins.currentSystem }:
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs { inherit system; };
+in
+pkgs.dockerTools.buildImage {
+  name = "the-press";
+  tag = "latest";
+  contents = with pkgs; [
+    (
+      haskellPackages.pandoc-citeproc
+        haskellPackages.pandoc-crossref
+        pandoc
+        texlive.combine
+        {
+          inherit (texlive)
+            collection-basic
+            collection-bibtexextra
+            collection-binextra
+            collection-fontsextra
+            collection-fontsrecommended
+            collection-fontutils
+            collection-langenglish
+            collection-langeuropean
+            collection-langitalian
+            collection-latex
+            collection-latexextra
+            collection-latexrecommended
+            collection-luatex
+            collection-mathscience
+            collection-pictures
+            collection-pstricks
+            collection-publishers
+            collection-xetex
+            ;
+        }
+    )
+  ];
+}
